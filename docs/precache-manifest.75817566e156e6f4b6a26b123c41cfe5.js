dd4024b2b6901f8b5b2347eb4fbbb49c635eb9d1
Danqing Liu <git@danqing.io>
2020-03-24 10:01:51 -0400

    Shift dates

--- docs/precache-manifest.75817566e156e6f4b6a26b123c41cfe5.js
+++ docs/precache-manifest.75817566e156e6f4b6a26b123c41cfe5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48620ae891751a80653b99115d2a33d3",
+    "revision": "7686a0648d883dfb46e75533b7caeedc",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.89f067aa.chunk.css"
   },
   {
-    "revision": "5bc165ed6e44b3a10caa",
+    "revision": "493ab980d3b0efc42728",
     "url": "/static/css/main.5e7f53f5.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.fc82687d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5bc165ed6e44b3a10caa",
-    "url": "/static/js/main.b1417667.chunk.js"
+    "revision": "493ab980d3b0efc42728",
+    "url": "/static/js/main.5980b939.chunk.js"
   },
   {
     "revision": "1c3db563b2d0b677ec5e",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.428a199e08e3131414d48ea4361570e7.js"
+  "/precache-manifest.75817566e156e6f4b6a26b123c41cfe5.js"
 );
 
 self.addEventListener('message', (event) => {
