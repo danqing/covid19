0a219280a8dce074c2629c27ab6d0df82a291bf3
Sid Palas <sid.palas@gmail.com>
2020-04-05 22:27:43 -0700

    only store the setUrl function

--- src/TwitterButton.tsx
+++ src/TwitterButton.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const TwitterButton = () => {
+  const setUrl = useState(window["location"].href)[1];
+
+  useEffect(() => {
+    setUrl(window["location"].href);
+  }, [window["location"].href]);
+
   return (
     <div>
       <a
